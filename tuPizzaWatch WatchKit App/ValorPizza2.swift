2d461aee2e48551b46547671f23c9d8834303d0e
Josep Palau Caballero <josep_palau@joseps-mbp.fritz.box>
2017-10-12 19:21:41 +0200

    segundo commit tu pizza watch

---
+++ tuPizzaWatch WatchKit App/ValorPizza2.swift
@@ -0,0 +1,24 @@
+//
+//  ValorPizza2.swift
+//  tuPizzaWatch
+//
+//  Created by Josep Palau Caballero on 10/10/2017.
+//  Copyright © 2017 palauturf. All rights reserved.
+//
+
+import WatchKit
+
+
+class ValorPizza2: NSObject {
+    
+    var valorTamaño: String = ""
+    
+    var valorMasa: String = ""
+    
+    init(v: String, v2: String) {
+        
+        valorTamaño = v
+        valorMasa = v2
+    }
+    
+}
